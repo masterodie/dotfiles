756fcd5f038f18bffae4c7f8e798f2bb750d998d
odie <masterodie@gmail.com>
2015-01-26 21:54:41 +0100

    Pulled newest changes

--- update.sh
+++ update.sh
@@ -1,27 +1,86 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
 
-#update git submodules
-git submodule init
-git submodule foreach git pull origin master
-git submodule update
+echo $DIR
 
-#Update VIM git submodules
-cd vim
-git submodule init
-git submodule foreach git pull origin master
-git submodule update
-cd ..
+#VIM files
+declare -A VIM_FILES
+VIM_FILES=( ['vim']='.vim' ['vim/vimrc']='.vimrc' )
 
-#update symlinks
-for i in *
+#ZSH Files
+declare -A ZSH_FILES
+ZSH_FILES=( ['zshrc']='.zshrc' ['zshrc.linux']='.zshrc.local' ['zshrc.macos']='.zshrc.local' )
+
+#OH MY ZSH
+declare -A OH_MY_ZSH_FILES
+OH_MY_ZSH_FILES=( ['oh-my-zsh']='.oh-my-zsh' ['zshrc.oh-my-zsh']='.zshrc.oh-my-zsh' )
+
+#Update git submodules
+function _update_git {
+git submodule update --init --recursive
+}
+
+#Create symlink
+function _create_zsh_symlinks {
+for key in ${!ZSH_FILES[@]}
 do
-        if [[ $i == "update.sh" ]]
-        then
-                continue
-        fi
-       ln -sf $DIR/$i $HOME/.$i
+        ln -s "${DIR}/${key}" "${HOME}/${ZSH_FILES[${key}]}"
 done
-ln -sf $DIR/vim/vimrc $HOME/.vimrc
+}
+
+function _create_oh_my_zsh_symlinks {
+for key in ${!OH_MY_ZSH_FILES[@]}
+do
+        ln -s ${DIR}/${key} ${HOME}/${OH_MY_ZSH_FILES[${key}]}
+done
+}
+
+function _create_vim_symlinks {
+for key in ${!VIM_FILES[@]}
+do
+        ln -s ${DIR}/${key} ${HOME}/${VIM_FILES[${key}]}
+done
+}
+
+#remove symlinks
+function _remove_zsh_symlinks {
+for key in ${!ZSH_FILES[@]}
+do
+        unlink ${ZSH_FILES[${key}]}
+done
+}
+
+function _remove_oh_my_zsh_symlinks {
+for key in ${!OH_MY_ZSH_FILES[@]}
+do
+        unlink ${OH_MY_ZSH_FILES[${key}]}
+done
+}
+
+function _remove_vim_symlinks {
+for key in ${!VIM_FILES[@]}
+do
+        unlink ${VIM_FILES[${key}]}
+done
+}
+
+#compile vim modules
+function _compile_vim_youcompleteme {
+if [ -e ~/.vim ]
+then
+        cd ~/.vim/bundle/youcompleteme/
+        ./install.sh --clang-completer --system-libclang --system-boost
+fi
+}
+
+function all {
+        cd $DIR
+        #_update_git
+        _create_vim_symlinks
+        _create_zsh_symlinks
+        _create_oh_my_zsh_symlinks
+        _compile_vim_youcompleteme
+}
+
+all
