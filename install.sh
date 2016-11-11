c2302eb979d6fbf551eb265c086503ad46925dfb
Patrick Neff <masterodie@gmail.com>
2016-11-11 02:26:17 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -8,16 +8,20 @@ git submodule update --init --recursive
 source _install/link.sh
 source _install/copy.sh
 
-#if [[ ! $SHELL =~ 'zsh' ]]
-#then
-    #echo "Configuring zsh as default shell"
-    #chsh -s $(which zsh)
-#else
-    #echo "zsh is already the default shell"
-#fi
+if [[ ! $SHELL =~ 'zsh' ]]
+then
+    if [[ -f $(which zsh) ]]
+    then
+        echo "Configuring zsh as default shell"
+        chsh -s $(which zsh)
+    else
+    fi
+else
+    echo "zsh is already the default shell"
+fi
 
 echo "Installing vim Plugins"
-$EDITOR -c PlugUpgrade -c PlugInstall -c PlugUpdate -c quitall
+vim -c PlugUpgrade -c PlugInstall -c PlugUpdate -c quitall
 
 echo "Done."
 exit 0
